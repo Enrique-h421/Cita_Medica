516687715b1ceb31e863054eb89523fe9dd335de
Peña <ph202402@upes.edu.sv>
2026-08-17 19:39:25 -0600

    Subir

---
+++ src/ClassLibrary1/AgendaYAtencion/EstadoCita.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.AgendaYAtencion
+{
+    public class EstadoCita
+    {
+        public int EstadoCitaID { get; set; }
+        public string Codigo { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+
+        
+        public ICollection<Cita> Citas { get; set; } = new List<Cita>();
+    }
+}
---
+++ src/Persistence/ClinicaDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Domain.AgendaYAtencion;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class ClinicaDbContext : DbContext
+    {
+       
+        public ClinicaDbContext(DbContextOptions<ClinicaDbContext> options) : base(options) { }
+
+       
+        public DbSet<Cita> Citas { get; set; }
+        public DbSet<EstadoCita> Estados_Cita { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+            modelBuilder.Entity<EstadoCita>().HasKey(e => e.EstadoCitaID);
+            modelBuilder.Entity<Cita>().HasKey(c => c.CitaID);
+
+            
+            modelBuilder.Entity<Cita>()
+                .HasOne(c => c.EstadoCita)
+                .WithMany(e => e.Citas)
+                .HasForeignKey(c => c.EstadoCitaID);
+        }
+    }
+}
